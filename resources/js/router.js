d4962d47ee063b7c68516b5166283c3b104abf44
sergei <s.kupriyanov@nextcontact.ru>
2024-03-21 23:25:37 +0300

    конфиг на прод

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory('https://laravel-task.ru'),
+    history: createWebHistory(''),
     routes: [
         {
             path: '/task',
